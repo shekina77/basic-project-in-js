1fa614822fedf83045669fce7e61a8334350f461
unknown <shekinadhanunjay@gmail.com>
2024-12-09 23:06:23 +0530

    basic project in js

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let input=document.getElementById("color")
+let input2=document.getElementById("bRadius")
+let image=document.querySelector("img");
+
+function colorChnage(z){
+    console.log(z);
+    
+     if(z.key==="Enter"){
+        document.body.style.backgroundcolor='input.value' 
+     }
+}
+
+
